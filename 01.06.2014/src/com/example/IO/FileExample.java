68585a91e2e9e5df0e8b1f31a4f5d970727c7a9c
kursad.aydin <kursad.aydin@KZL-JA38402-B11.baogrenci.com>
2014-06-01 11:57:02 +0300

    IO

--- 01.06.2014/src/com/example/IO/FileExample.java
+++ 01.06.2014/src/com/example/IO/FileExample.java
@@ -2,15 +2,29 @@ package com.example.IO;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileExample {
 
 	public static void main(String[] args) throws IOException {
 		
-		File files = new File("C:\\Users\\kursad.aydin\\Desktop\\JavaIO\\text.xls");
-		files.createNewFile();
-		System.out.println(files.getParent());
-		System.out.println(files.canRead());
+		ArrayList<String> fileDummy = new ArrayList<String>();
+		
+		File files = new File("C:\\Users\\kursad.aydin\\Desktop\\JavaIO");
+		/*files.createNewFile();
+		System.out.println(files.getParent());*/
+		
+		File [] fileList = files.listFiles();
+		
+		for (int i = 0; i < fileList.length; i++) {
+			
+			if (fileList[i].isDirectory()){
+				
+				fileDummy.add(fileList[i].getName());
+			}
+			
+			System.out.print(fileDummy);
+		}
 		
 	}
 
